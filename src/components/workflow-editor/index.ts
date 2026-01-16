60db7d4769b3b72c4c6c45adbefd3f1541a9576a
Jimmy <acefengyun@gmail.com>
2026-01-16 17:45:46 +0800

    feat: add workflow editor exports

---
+++ src/components/workflow-editor/index.ts
@@ -0,0 +1,11 @@
+export { WorkflowEditor } from './WorkflowEditor'
+export { useWorkflowStore } from './stores/use-workflow-store'
+export type {
+  WorkflowNodeData,
+  WorkflowEdgeData,
+  WorkflowNodeType,
+  ApproverType,
+  OrgRelation,
+  ParallelMode,
+  ConditionOperator,
+} from './stores/use-workflow-store'
