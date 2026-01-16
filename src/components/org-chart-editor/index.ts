9503e2ce5e4e1be0d911b78b5fc3ede34711c95b
Jimmy <acefengyun@gmail.com>
2026-01-16 17:14:48 +0800

    feat: add org chart editor exports

---
+++ src/components/org-chart-editor/index.ts
@@ -0,0 +1,3 @@
+export { OrgChartEditor } from './OrgChartEditor'
+export { useOrgChartStore } from './stores/use-org-chart-store'
+export type { OrgNodeData, OrgEdgeData, OrgNodeType, OrgRelationType } from './stores/use-org-chart-store'
